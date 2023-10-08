92f36af4eb4c375d6124b00a4b1c5ade857e566d
shunmaru <programming.learn226@gmail.com>
2023-10-08 07:36:51 +0000

    mentor

---
+++ mentor.rb
@@ -0,0 +1,39 @@
+class Mentor
+  attr_accessor :name
+  
+  def initialize
+    self.name = name
+  end
+  
+  def name
+    '煌木'
+  end
+  
+  def job
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+  end
+end
+
+class RailsMentor < Mentor
+  
+  @@name = '赤出'
+  
+  def initialize
+    self.name = name
+  end
+  
+  def name
+    @@name
+  end
+  
+  def job
+    puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+  end
+  
+end
+
+kirameki = Mentor.new
+akaide = RailsMentor.new
+
+kirameki.job
+akaide.job
